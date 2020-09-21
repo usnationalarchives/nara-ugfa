90d2de5dbbfb48447e92b4f523215684792cd324
Daniel Boggs <daniel.boggs@gmail.com>
2020-09-21 12:10:46 -0400

    feat: add styles for "add section" feature

--- app/javascript/src/components/shared/PlusCircle.js
+++ app/javascript/src/components/shared/PlusCircle.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import styled, { css } from 'styled-components';
+import styled, { css } from "styled-components";
 
-const Root = styled.div`
-  background: ${props => props.theme.colors.green};
+const PlusCircle = styled.div`
+  background: ${(props) => props.theme.colors.green};
   border-radius: 100%;
   display: inline-block;
   height: 28px;
@@ -11,34 +10,33 @@ const Root = styled.div`
   vertical-align: middle;
   width: 28px;
 
+  ${(props) =>
+    props.grey &&
+    css`
+      background: ${(props) => props.theme.colors.textLightGrey};
+    `}
+
   &:before,
-  &:after{
-    background: ${props => props.theme.colors.white};
+  &:after {
+    background: ${(props) => props.theme.colors.white};
     bottom: 3px;
-    content: ''; 
-    left: 3px; 
-    position: absolute; 
-    right: 3px; 
-    top: 3px; 
+    content: "";
+    left: 3px;
+    position: absolute;
+    right: 3px;
+    top: 3px;
   }
 
-  &:before{
+  &:before {
     width: 2px;
     margin: 3px auto;
   }
-  &:after{
+
+  &:after {
     margin: auto 3px;
     height: 2px;
     box-shadow: none;
   }
 `;
-
-
-const PlusCircle = () => {
-  return (
-    <Root>
-    </Root>
-  );
-};
 
 export default PlusCircle;
