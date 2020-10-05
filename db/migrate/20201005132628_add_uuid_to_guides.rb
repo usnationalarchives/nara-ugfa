ed690d8a6957537d7eb233df27138f9af4a4f6dd
Daniel Boggs <daniel.boggs@gmail.com>
2020-10-05 11:25:38 -0400

    hotfix: reset column information before assigning legacy UUIDs

--- db/migrate/20201005132628_add_uuid_to_guides.rb
+++ db/migrate/20201005132628_add_uuid_to_guides.rb
@@ -2,6 +2,8 @@ class AddUuidToGuides < ActiveRecord::Migration[6.0]
   def change
     add_column :guides, :uuid, :uuid, index: true
 
+    Guide.reset_column_information
+
     Guide.all.each do |guide|
       guide.save(validate:false)
     end
