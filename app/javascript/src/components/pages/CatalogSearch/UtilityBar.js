268f06827484dbef3951ce1cb234e384e5576e05
Daniel Boggs <daniel.boggs@gmail.com>
2020-10-26 13:30:30 -0400

    qa: #156, remove legacy catalog component

--- app/javascript/src/components/pages/CatalogSearch/UtilityBar.js
+++ app/javascript/src/components/pages/CatalogSearch/UtilityBar.js
@@ -1,12 +1,12 @@
 import React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 // components
 import * as Layout from "#components/shared/Layout";
 
 export const Root = styled.div`
-  border-bottom: 1px solid ${props => props.theme.colors.mediumGrey};
+  border-bottom: 1px solid ${(props) => props.theme.colors.mediumGrey};
   display: flex;
   font-size: 0.9em;
   justify-content: space-between;
@@ -18,8 +18,9 @@ export const Utilties = styled.div`
   justify-content: space-between;
   max-width: ${(props) => props.theme.layout.maxWidth};
   padding: 10px 0;
-  
-  @media all and (min-width: ${(props) => props.theme.layout.catalogColumnMin}) {
+
+  @media all and (min-width: ${(props) =>
+      props.theme.layout.catalogColumnMin}) {
     flex-direction: row;
     padding: 10px 0;
 
@@ -44,25 +45,24 @@ export const ShareLinks = styled.ul`
     }
   }
 
-  @media all and (min-width: ${(props) => props.theme.layout.catalogColumnMin}) {
+  @media all and (min-width: ${(props) =>
+      props.theme.layout.catalogColumnMin}) {
     margin-bottom: 0;
   }
 `;
-
 
 const UtilityBar = () => {
   return (
     <Root>
       <Layout.Padding>
         <Layout.Wrapper>
-        <Utilties>
-          <p>1-20 of 345,238 results in 1.38 seconds. Show expanded search terms.</p>
-          <ShareLinks>
-            <li>Share |</li>
-            <li>Export |</li>
-            <li>Print</li>
-          </ShareLinks>
-        </Utilties>
+          <Utilties>
+            <ShareLinks>
+              <li>Share |</li>
+              <li>Export |</li>
+              <li>Print</li>
+            </ShareLinks>
+          </Utilties>
         </Layout.Wrapper>
       </Layout.Padding>
     </Root>
